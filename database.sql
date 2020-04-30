35eecf56ca4df97d5a2ba45280a1f2751925d89a
Scott Bromander <scottbromander@gmail.com>
2020-04-29 21:21:38 -0500

    updates SQL file to be accurate

--- database.sql
+++ database.sql
@@ -1,18 +1,36 @@
-CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+CREATE TABLE "user"
+(
+	"id" SERIAL PRIMARY KEY,
+	"username" VARCHAR (80) UNIQUE NOT NULL,
+	"password" VARCHAR (1000) NOT NULL,
+	"team" INT REFERENCES "team"
 );
 
-CREATE TABLE "challenge" (
+CREATE TABLE "challenge"
+(
 	"id" SERIAL PRIMARY KEY,
-	"name" VARCHAR (80),
 	"description" VARCHAR (2400)
 );
 
-CREATE TABLE "submission" (
+CREATE TABLE "submission"
+(
 	"id" SERIAL PRIMARY KEY,
 	"user" INT REFERENCES "user",
-	"challenge" INT REFERENCES "challenge", 
-	"image_url" VARCHAR (240)
+	"challenge_id" INT REFERENCES "challenge",
+	"image_url" VARCHAR (240),
+	"status" INT REFERENCES "status",
+	"team_id" INT REFERENCES "team"
 );
+
+CREATE TABLE "team"
+(
+	"id" SERIAL PRIMARY KEY,
+	"name" VARCHAR(80)
+);
+
+CREATE TABLE "status"
+(
+	"id" SERIAL PRIMARY KEY,
+	"status_name" VARCHAR(16)
+);
+
