32acaa76d3a2f408d2592fdeffd0b77a3f469b45
Scott Bromander <scottbromander@gmail.com>
2020-05-03 09:59:10 -0500

    uno mas deploy test

--- src/components/Pages/UserPage/UserPage.js
+++ src/components/Pages/UserPage/UserPage.js
@@ -49,7 +49,7 @@ class UserPage extends Component {
       (item, index) => {
         let button = (
           <button
-            className="btn btn-primary"
+            className="btn btn-primary btn-sm"
             onClick={this.toggleModal(true, item)}
           >
             Submit
@@ -61,16 +61,20 @@ class UserPage extends Component {
             switch (challenge['status_name']) {
               case 'pending':
                 button = (
-                  <button className="btn btn-warning" disabled>
+                  <button className="btn btn-warning btn-sm" disabled>
                     Submitted
                   </button>
                 );
                 break;
               case 'approved':
-                button = <button className="btn btn-success">Accepted!</button>;
+                button = (
+                  <button className="btn btn-success btn-sm">Accepted!</button>
+                );
                 break;
               case 'declined':
-                button = <button className="btn btn-danger">Rejected</button>;
+                button = (
+                  <button className="btn btn-danger btn-sm">Rejected</button>
+                );
                 break;
               default:
                 break;
