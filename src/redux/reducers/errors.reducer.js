87e4035edf6a0a2a820efa2bf391b8c4fc1d2fb3
Scott Bromander <scottbromander@gmail.com>
2020-05-05 15:38:07 -0500

    Error messages integrated

--- src/redux/reducers/errors.reducer.js
+++ src/redux/reducers/errors.reducer.js
@@ -7,11 +7,11 @@ const loginMessage = (state = '', action) => {
     case 'CLEAR_LOGIN_ERROR':
       return '';
     case 'LOGIN_INPUT_ERROR':
-      return 'Enter your username and password!';
+      return 'LOGIN_INPUT_ERROR';
     case 'LOGIN_FAILED':
-      return 'Oops! The username and password didn\'t match. Try again!';
+      return 'LOGIN_FAILED';
     case 'LOGIN_FAILED_NO_CODE':
-      return 'Oops! Something went wrong! Is the server running?';
+      return 'LOGIN_FAILED_NO_CODE';
     default:
       return state;
   }
@@ -24,9 +24,9 @@ const registrationMessage = (state = '', action) => {
     case 'CLEAR_REGISTRATION_ERROR':
       return '';
     case 'REGISTRATION_INPUT_ERROR':
-      return 'Choose a username and password!';
+      return 'REGISTRATION_INPUT_ERROR';
     case 'REGISTRATION_FAILED':
-      return 'Oops! That didn\'t work. The username might already be taken. Try again!';
+      return 'REGISTRATION_FAILED';
     default:
       return state;
   }
