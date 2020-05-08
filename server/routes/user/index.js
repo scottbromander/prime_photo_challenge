81573e1cfeb62ed1c9e108852502b986e54a919e
Scott Bromander <scottbromander@gmail.com>
2020-05-08 10:26:22 -0500

    clean up

--- server/routes/user/index.js
+++ server/routes/user/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
+
+//Data
+const pool = require('../../modules/pool');
+const redisClient = require('../../modules/redis-store');
+
+// Forgot Password
+const makeid = require('../../modules/password-reset');
+const mailerForgotPassword = require('../../modules/mailer-forgot-password');
+
+// Auth
+const encryptLib = require('../../modules/encryption');
+const userStrategy = require('../../strategies/user.strategy');
 const {
   rejectUnauthenticated,
 } = require('../../modules/authentication-middleware');
-const encryptLib = require('../../modules/encryption');
-const pool = require('../../modules/pool');
-const userStrategy = require('../../strategies/user.strategy');
-const passwordReset = require('../../modules/password-reset');
-const moment = require('moment');
 
 const router = express.Router();
 
@@ -31,50 +38,58 @@ module.exports = (params) => {
     res.sendStatus(200);
   });
 
-  router.post('/forgot', (req, res, next) => {
-    passwordReset(res, req.body.email);
+  router.post('/forgot', async (req, res, next) => {
+    const findUserQueryString = `SELECT * FROM "user" WHERE "email"=$1;`;
+
+    try {
+      const response = await pool.query(findUserQueryString, [req.body.email]);
+      if (response.rows == 0) res.send(200);
+
+      const user = response.rows[0];
+      const hex = makeid(8); // 8 character hex code
+
+      redisClient
+        .hmset(user.username, { ['hex']: hex })
+        .then(async (result) => {
+          redisClient.expire(user.username, 60 * 5); // 60 seconds * 5 = 5 minutes
+
+          try {
+            await mailerForgotPassword(user, hex);
+            res.sendStatus(200);
+          } catch (error) {
+            res.sendStatus(500);
+          }
+        })
+        .catch((err) => res.sendStatus(500));
+    } catch (error) {
+      res.sendStatus(500);
+    }
   });
 
   router.put('/reset/password', (req, res, next) => {
     if (!req.body.hex || req.body.hex.length != 8) res.sendStatus(401);
-
     const hex = req.body.hex;
-    const queryString = `SELECT * FROM "password_reset" WHERE "password_reset"."hex"=$1;`;
 
-    pool
-      .query(queryString, [hex])
+    redisClient
+      .hget(req.body.user, 'hex')
       .then((response) => {
-        if (response.rows.length === 0) {
+        if (
+          !response ||
+          response !== hex ||
+          !req.body.newPassword ||
+          !req.body.user
+        )
           return res.sendStatus(401);
-        }
 
-        const resetKey = response.rows[0];
-        const generatedTime = moment(resetKey.date); // timestamp of key
-        const currentTime = moment(Date.now()); // current time
-        const diff = generatedTime.diff(currentTime, 'minutes'); // find the difference
-        const timeDiff = -10; // threshold in minutes
+        const newPassword = encryptLib.encryptPassword(req.body.newPassword);
+        const updateQueryString = `UPDATE "user" SET "password"=$1 WHERE "username"=$2;`;
 
-        if (diff > timeDiff && req.body.newPassword && req.body.user) {
-          const newPassword = encryptLib.encryptPassword(req.body.newPassword);
-          const updateQueryString = `UPDATE "user" SET "password"=$1 WHERE "username"=$2;`;
-
-          pool
-            .query(updateQueryString, [newPassword, req.body.user])
-            .then((response) => {
-              res.sendStatus(200);
-            })
-            .catch((err) => {
-              console.warn(`Error saving new password: ${err}`);
-              res.sendStatus(500);
-            });
-        } else {
-          res.sendStatus(401); // missing part of the reset cred
-        }
+        pool
+          .query(updateQueryString, [newPassword, req.body.user])
+          .then((response) => res.sendStatus(200))
+          .catch((err) => res.sendStatus(500));
       })
-      .catch((err) => {
-        console.warn(err);
-        res.sendStatus(500);
-      });
+      .catch((err) => res.sendStatus(500));
   });
 
   router.post('/logout', (req, res) => {
